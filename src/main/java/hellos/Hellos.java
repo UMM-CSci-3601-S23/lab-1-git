424f53c9d880b5c2bc411c522047ee4b58dfc45e
K.K. Lamberty <lamberty@morris.umn.edu>
2023-01-16 21:25:41 -0600

    Initial commit

---
+++ src/main/java/hellos/Hellos.java
@@ -0,0 +1,36 @@
+package hellos;
+
+/**
+ * A simple Java program used to demonstrate merge conflicts when
+ * multiple people edit the same piece of code.
+ */
+public class Hellos {
+
+  public static void main(final String[] args) {
+    Hellos helloRunner = new Hellos();
+    helloRunner.run();
+  }
+
+  public void run() {
+    String output = generateOutput();
+    System.out.println(output);
+  }
+
+  public String generateOutput() {
+    StringBuilder builder = new StringBuilder();
+
+    builder.append(kkSaysHello());
+    builder.append(nicSaysHello());
+
+    return builder.toString();
+  }
+
+  private String nicSaysHello() {
+    return "Nic says 'Howdy!'\n";
+  }
+
+  private String kkSaysHello() {
+    return "KK says 'Hello!'\n";
+  }
+
+}
